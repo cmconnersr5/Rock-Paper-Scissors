20b2d7138bf665b1d707d700b6ee55ab2f61b470
cmconnersr5 <121987232+cmconnersr5@users.noreply.github.com>
2023-02-04 17:50:00 -0500

    Add refined button functionality

--- index.js
+++ index.js
@@ -1,62 +1,84 @@
 //to-do: 
-// fix the computer choice string display
 // add result display
-// add button effects and styles
-// stye computer choice display
+// add single iteration functionality / disable buttons after click
 
 let rock = document.querySelector('#btn1')
     rock.addEventListener('click', playRock)
     function playRock(){
+        const but = document.querySelector('#btn1');
+        but.classList.add('selected');
         let playerSelection="rock";
         let computerSelection=Math.random()
             if (computerSelection <= 0.34){
             computerSelection="rock";
+            const but = document.querySelector('#btn4');
+            but.classList.add('selected');
             } else if (computerSelection <= 0.67){
             computerSelection="paper";
+            const but = document.querySelector('#btn5');
+            but.classList.add('selected');
             } else {
             computerSelection="scissors";
-            }
-        const container = document.getElementById('compChoice');
-        const text = document.createTextNode(computerSelection);
-        container.appendChild(text);    
+            const but = document.querySelector('#btn6');
+            but.classList.add('selected');
+            }  
         playRound(playerSelection,computerSelection);
     }
     
 let paper = document.querySelector('#btn2')
     paper.addEventListener('click', playPaper)
     function playPaper() {
+        const but = document.querySelector('#btn2');
+        but.classList.add('selected');
         let playerSelection="paper";
         let computerSelection=Math.random()
             if (computerSelection <= 0.34){
             computerSelection="rock";
+            const but = document.querySelector('#btn4');
+            but.classList.add('selected');
             } else if (computerSelection <= 0.67){
             computerSelection="paper";
+            const but = document.querySelector('#btn5');
+            but.classList.add('selected');
             } else {
             computerSelection="scissors";
+            const but = document.querySelector('#btn6');
+            but.classList.add('selected');
             }
-        const container = document.getElementById('compChoice');
-        const text = document.createTextNode(computerSelection);
-        container.appendChild(text);
         playRound(playerSelection,computerSelection);
     }
 
 let scissors = document.querySelector('#btn3')
     scissors.addEventListener('click', playScissors)
     function playScissors (){
+        const but = document.querySelector('#btn3');
+        but.classList.add('selected');
         let playerSelection="scissors";
         let computerSelection=Math.random()
             if (computerSelection <= 0.34){
             computerSelection="rock";
+            const but = document.querySelector('#btn4');
+            but.classList.add('selected');
             } else if (computerSelection <= 0.67){
             computerSelection="paper";
+            const but = document.querySelector('#btn5');
+            but.classList.add('selected');
             } else {
             computerSelection="scissors";
-            }
-        const container = document.getElementById('compChoice');
-        const text = document.createTextNode(computerSelection);
-        container.appendChild(text);    
+            const but = document.querySelector('#btn6');
+            but.classList.add('selected');
+            } 
         playRound(playerSelection,computerSelection);
     }
+
+const playAgain = document.querySelector('#btn7')
+    playAgain.addEventListener('click', () => {
+        const buttonAll = document.querySelectorAll('.button');
+        buttonAll.forEach(button => {
+            button.classList.remove('selected');
+        });
+        
+    });
 
 function playRound(playerSelection, computerSelection) {  
     if (playerSelection == computerSelection){
@@ -82,6 +104,7 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+ 
 
 //generate computer selection randomly
 // let computerSelection=Math.random()
